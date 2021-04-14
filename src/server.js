341fba22806d1d86c03756f64d0add608022f2f3
Kurt Obando <kurtobando.developer@gmail.com>
2021-04-14 12:49:03 +0800

    initial commit for emmit, and listen from socket io

--- src/server.js
+++ src/server.js
@@ -1,21 +1,29 @@
 require("dotenv").config()
 
-const { PORT } = require("./_variables")
-const cors = require("cors")
 const compression = require("compression")
 const helmet = require("helmet")
 const express = require("express")
 const app = express()
+const http = require("http").createServer(app)
+const io = require("socket.io")(http, {
+    cors: {
+        origin: ["http://localhost:8000", "http://localhost:3000", "http://localhost"],
+        methods: ["GET", "POST"],
+    },
+})
+const { PORT } = require("./_variables")
+const socketIO = require("./_socket-io")
 
 app.use(helmet())
 app.use(compression())
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(require("./routes/index"))
 app.use(require("./routes/authenticate"))
 
-app.listen(PORT, () => {
+socketIO(io)
+
+http.listen(PORT, () => {
     console.log(`[app] can be access via http://localhost:${PORT}`)
 })
