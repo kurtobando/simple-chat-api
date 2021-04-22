27ee45fd1bcadec66ac6a664d2c4be45c94fcb36
Kurt Obando <kurtobando.developer@gmail.com>
2021-04-22 13:30:59 +0800

    remove console.log(), and improve emit for disconnect and USER_DISCONNECT events

--- src/_socket-io.js
+++ src/_socket-io.js
@@ -3,48 +3,51 @@ const socketIO = {
         let onlineUsers = {}
 
         io.on("connection", (socket) => {
-            console.log("[server] user connected, with an id", socket.id)
-
             socket.on("disconnect", () => {
-                console.log("[server] user disconnected", socket.id)
                 const userDisconnect = onlineUsers[socket.id]
-                delete onlineUsers[socket.id]
 
-                // emit
-                io.emit("ONLINE_USERS", onlineUsers)
-                io.emit("USER_DISCONNECTED", userDisconnect)
+                if (userDisconnect !== undefined) {
+                    delete onlineUsers[socket.id]
+                    // emit
+                    io.emit("ONLINE_USERS", onlineUsers)
+                    io.emit("USER_DISCONNECTED", userDisconnect)
+                }
             })
 
             socket.on("disconnecting", (reason) => {
-                console.log(`[server] user disconnecting, reason: ${reason}`)
+                return false
             })
 
             socket.on("USER_CONNECTED", (args) => {
                 onlineUsers[socket.id] = args
-                console.log(`[server] received from client ${JSON.stringify(args)}`)
 
                 // emit
                 io.emit("USER_CONNECTED", args)
                 io.emit("ONLINE_USERS", onlineUsers)
             })
 
-            socket.on("USER_SEND_MESSAGE", (data) => {
-                console.log(`[server] user send message`, data)
+            socket.on("USER_DISCONNECTED", () => {
+                const userDisconnect = onlineUsers[socket.id]
 
+                if (userDisconnect !== undefined) {
+                    delete onlineUsers[socket.id]
+                    // emit
+                    io.emit("ONLINE_USERS", onlineUsers)
+                    io.emit("USER_DISCONNECTED", userDisconnect)
+                }
+            })
+
+            socket.on("USER_SEND_MESSAGE", (data) => {
                 // emit
                 socket.broadcast.emit("USER_SEND_MESSAGE", data)
             })
 
             socket.on("USER_TYPING", (data) => {
-                console.log(`[server] user typing`, data)
-
                 // emit
                 socket.broadcast.emit("USER_TYPING", data)
             })
 
             socket.on("USER_NOT_TYPING", (data) => {
-                console.log(`[server] user not typing`, data)
-
                 // emit
                 socket.broadcast.emit("USER_NOT_TYPING", data)
             })
